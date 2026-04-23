d402f099df3821a5b38417056834531857469c2f
ItokianaIZ07 <itokianarabarivelo@gmail.com>
2026-04-23 07:13:21 +0300

    [CORE] ajout logique de télechargement

--- core/downloader.py
+++ core/downloader.py
@@ -1,15 +1,40 @@
 import subprocess
+import re
 
 
 class Downloader:
-
     def __init__(self):
-        self.__downloader = "./bin/yt-dlp.exe"
+        self.yt_dlp_path = "./bin/yt-dlp.exe"
 
-    def download_video(self, url, format="mp4"):
+    def download(self, url, format="mp4", output_path=".", progress_callback=None):
+        command = [self.yt_dlp_path, "--newline"]
+
+        # Format
         if format == "mp3":
-            cmd = [f"{self.__downloader}", "-x", "--audio-format", "mp3", url]
+            command += ["-x", "--audio-format", "mp3"]
         else:
-            cmd = [f"{self.__downloader}", "-f", "mp4", url]
+            command += ["-f", "mp4"]
 
-        subprocess.run(cmd)
+        # Output path
+        command += ["-o", f"{output_path}/%(title)s.%(ext)s"]
+
+        command.append(url)
+
+        process = subprocess.Popen(
+            command,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True
+        )
+
+        for line in process.stdout:
+            percent = self._extract_progress(line)
+
+            if percent is not None and progress_callback:
+                progress_callback(percent)
+
+    def _extract_progress(self, text):
+        match = re.search(r'(\d+\.\d+)%', text)
+        if match:
+            return float(match.group(1)) / 100  # 0 → 1
+        return None
